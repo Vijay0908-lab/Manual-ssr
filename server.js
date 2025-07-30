f3697e51c6403010ad6977b63ebe807b8f969f94
Vijay0908-lab <chauhanvijay0908@gmail.com>
2025-07-30 13:34:57 +0530

    new files are added

---
+++ server.js
@@ -0,0 +1,83 @@
+const { readFileSync } = require("fs");
+const { createServer } = require("http");
+const { parse } = require("url");
+const { renderToString } = require("react-dom/server");
+
+//need to download the npm i -D @babel/core @babel/preset-env @babel/preset-react @babel/register for the proper functioning of the project especialy when these type data is included  also need to load the react and react-dom
+const pizzas = [
+  {
+    name: "Focaccia",
+    price: 6,
+  },
+  {
+    name: "Pizza Margherita",
+    price: 10,
+  },
+  {
+    name: "Pizza Spinaci",
+    price: 12,
+  },
+  {
+    name: "Pizza Funghi",
+    price: 12,
+  },
+  {
+    name: "Pizza Prosciutto",
+    price: 15,
+  },
+];
+
+function Home() {
+  return (
+    <div>
+      <h1>🍕 Fast React Pizza Co.</h1>
+      <p>This page has been rendered with React on the server 🤯</p>
+
+      <h2>Menu</h2>
+      <ul>
+        {pizzas.map((pizza) => (
+          <MenuItem pizza={pizza} key={pizza.name} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = React.useState(0);
+  return (
+    <div>
+      <button onClick={() => setCount((c) => c + 1)}>+1</button>
+      <span>{count}</span>
+    </div>
+  );
+}
+
+function MenuItem({ pizza }) {
+  return (
+    <li>
+      <h4>
+        {pizza.name} (${pizza.price})
+      </h4>
+      <Counter />
+    </li>
+  );
+}
+
+const htmlTemplaten = readFileSync(`${__dirname}/index.html`, "utf-8");
+
+const server = createServer((req, res) => {
+  const pathName = parse(req.url, true).pathname;
+
+  if (pathName === "/") {
+    res.writeHead(200, { "Content-type": "text/html" });
+
+    res.end(htmlTemplaten);
+  } else if (pathName === "/test") {
+    res.end("test");
+  } else {
+    res.end("this url cannot be found");
+  }
+});
+
+server.listen(8010, () => console.log("listening for requests on port 8010"));
